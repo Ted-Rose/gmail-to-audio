280c03ce7d6842bada12bb8cd16dcd4d63636744
Tedis Rozenfelds <75977150+Ted-Rose@users.noreply.github.com>
2024-06-07 13:04:55 +0300

    Fetch gmail labels, output as audio

--- django/coding_diary.md
+++ django/coding_diary.md
@@ -1,3 +1,5 @@
 # Development logs
-1. `http://127.0.0.1:8000/` outputs 'Hello, World!'
+4. `http://127.0.0.1:8000/` outputs authenticated Gmail labels
+3. Following Google Workspace [tutorial](https://developers.google.com/gmail/api/quickstart/python)
 2. `source env/bin/activate`
+1. `http://127.0.0.1:8000/` outputs 'Hello, World!'
--- django/gmail/views.py
+++ django/gmail/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from gmail.utils import text_to_audio
+from gmail.utils import text_to_audio, get_labels
 
 
 def index(request):
-    audio_url = text_to_audio('Hello, World!', 'en')
+    print("Calling get labels")
+    labels = get_labels()
+    print("labels in index:", labels)
+    print("Calling get text_to_audio")
+    audio_url = text_to_audio(labels, 'en')
     return render(request, 'index.html', {'audio_url': audio_url})
